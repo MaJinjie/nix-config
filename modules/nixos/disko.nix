cb781a76d2218cf22fe6b7d9dc3b02fc61777ef3
MaJinjie <2734883936@qq.com>
2025-04-22 12:54:07 +0800

    fix: disko

--- modules/nixos/disko.nix
+++ modules/nixos/disko.nix
@@ -1,56 +1,52 @@
-_: 
-
-{
-disko.devices = {
-  disk = {
-    main = {
-      type = "disk";
-      device = "/dev/nvme0n1";
-      content = {
-        type = "gpt";
-        partitions = {
-          ESP = {
-            priority = 1;
-            name = "ESP";
-            start = "1M";
-            end = "512M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = ["umask=0077"];
+_: {
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              priority = 1;
+              name = "ESP";
+              start = "1M";
+              end = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = ["umask=0077"];
+              };
             };
-          };
-          NIXOS = {
-            size = "100%";
-            content = {
-              type = "btrfs";
-              extraArgs = ["-f"]; # Override existing partition
-              subvolumes = {
-                "@root" = {
-                  mountpoint = "/";
-                };
-                "@home" = {
-                  mountOptions = ["compress=zstd"];
-                  mountpoint = "/home";
-                };
-                "@nix" = {
-                  mountOptions = [
-                    "compress=zstd"
-                    "noatime"
-                  ];
-                  mountpoint = "/nix";
-                };
-                "@snap" = {
-                  mountOptions = ["compress=zstd"];
-                  mountpoint = "/snap";
-                };
-                "@swap" = {
-                  mountpoint = "/.swapvol";
-                  swap = {
-                    swapfile1.size = "8G";
-                    swapfile2.size = "8G";
+            NIXOS = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = ["-f"]; # Override existing partition
+                subvolumes = {
+                  "/root" = {
+                    mountpoint = "/";
+                  };
+                  "/home" = {
+                    mountOptions = ["compress=zstd"];
+                    mountpoint = "/home";
+                  };
+                  "/nix" = {
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                    mountpoint = "/nix";
+                  };
+                  "/snap" = {
+                    mountOptions = ["compress=zstd"];
+                    mountpoint = "/snap";
+                  };
+                  "/swap" = {
+                    mountpoint = "/.swapvol";
+                    swap.swapfile.size = "16G";
                   };
                 };
               };
@@ -60,5 +56,4 @@ disko.devices = {
       };
     };
   };
-};
 }
